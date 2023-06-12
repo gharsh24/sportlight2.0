4990cab036b73265f83259dbc7cb98ed9d0d8dc2
Harsh Goyal <harsh.sg360@gmail.com>
2023-06-12 22:54:26 +0530

    change font to tinos and montserrat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import Highlight from "./Highlight";
 import Description from "./Description";
 import Latest from "./latest";
 import { motion, useScroll, useSpring } from "framer-motion";
-import "./styles.css";
+import "../css/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
 
       <Description />
       <Highlight
-        src="https://cdn.pixabay.com/photo/2017/08/04/01/52/karate-2578819_1280.jpg"
+        src="https://cdn.pixabay.com/photo/2020/03/10/16/47/moon-4919501_640.jpg"
         heading="Hello World"
         news="The quick brown fox jumps over the lazy dog and finds a hidden treasure beneath the ancient oak tree near the sparkling river."
       />
--- src/components/Highlight.jsx
+++ src/components/Highlight.jsx
@@ -21,9 +21,10 @@ function Highlight(props) {
           </div>
         </Col>
         <Col className="d-flex align-items-center ">
-          <div>
-            <h1 className="text-left mx-auto" style={{fontWeight:"600"}}>{props.heading}</h1>
-            <p className="text-left mx-auto">{props.news}</p>
+          <div style={{fontFamily:"'Tinos','sans-serif'",}}>
+            <h1 className="text-left mx-auto" style={{fontWeight:"600"}}>
+              {props.heading}</h1>
+            <p className="text-left mx-auto" style={{fontSize: "1.2rem"}}>{props.news}</p>
           </div>
         </Col>
       </Row>
