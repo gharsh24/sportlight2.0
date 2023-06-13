d084a9860c53d779b00518d6e0be6736a4e37e42
Neeraj Sharma <neeraj716sharma@gmail.com>
2023-06-13 19:46:25 +0530

    Fixed Footer Conflict

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,55 +1,63 @@
-<<<<<<< HEAD
-import "../css/footer.css"
-=======
-import "./footer.css"
->>>>>>> 45483ae0fe5f3931dddd244349be56726e268458
+import "./footer.css";
 
-function footer(){
+function footer() {
   return (
     <div>
-      <div className="section5" style={{ marginTop: '4rem' }} >
-  <section>
-    <h1 className="title inline-photo2 show-on-scroll2">Subscribe to our newsletter</h1>
-    <p className="inline-photo show-on-scroll">Get the latest news on sports events, match results, player transfers, and upcoming tournaments.</p>
-  </section>
-  <section>
-    <button className="btn2">Subscribe</button>
-  </section>
-</div>
-
-    <footer>
-
-      <div className="footer">
+      <div className="section5" style={{ marginTop: "4rem" }}>
         <section>
-          <h3>XRIGADE</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <h1 className="title inline-photo2 show-on-scroll2">
+            Subscribe to our newsletter
+          </h1>
+          <p className="inline-photo show-on-scroll">
+            Get the latest news on sports events, match results, player
+            transfers, and upcoming tournaments.
+          </p>
         </section>
         <section>
-          <h3>LINKS</h3>
-          <a href="#">Home</a>
-          <a href="#">About Us</a>
-          <a href="#">Blog</a>
-          <a href="#">Contact Us</a>
-        </section>
-        <section>
-          <h3>OTHER</h3>
-          <a href="#">Privacy Policy</a>
-          <a href="#">Terms &amp; Conditions</a>
-          <a href="#">Cookie Policy</a>
-        </section>
-        <section>
-          <h3>ANY QUESTIONS?</h3>
-          <a href="emailto:support@websitename.com"><span><i className="fa fa-envelope" /></span><span>support@websitename.com</span></a>
-          <a href="tel:xxxxxxxxx"><span><i className="fa fa-phone" /></span><span>(+x)-xxx-xxxx-xxxxx</span></a>
+          <button className="btn2">Subscribe</button>
         </section>
       </div>
-    </footer>
-    <div className="sub-footer">
-      Copyright ©2023 All rights reserved | website name
+
+      <footer>
+        <div className="footer">
+          <section>
+            <h3>Sportslight</h3>
+            <p>Your one stop solution for sports craving</p>
+          </section>
+          <section>
+            <h3>LINKS</h3>
+            <a href="#">Home</a>
+            <a href="#">About Us</a>
+            <a href="#">Blog</a>
+            <a href="#">Contact Us</a>
+          </section>
+          <section>
+            <h3>OTHER</h3>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms &amp; Conditions</a>
+            <a href="#">Cookie Policy</a>
+          </section>
+          <section>
+            <h3>ANY QUESTIONS?</h3>
+            <a href="emailto:amanmuhal02@gmail.com">
+              <span>
+                <i className="fa fa-envelope" />
+              </span>
+              <span>amanmuhal02@gmail.com</span>
+            </a>
+            <a href="tel:xxxxxxxxx">
+              <span>
+                <i className="fa fa-phone" />
+              </span>
+              <span>(+x)-xxx-xxxx-xxxxx</span>
+            </a>
+          </section>
+        </div>
+      </footer>
+      <div className="sub-footer">
+        Copyright ©2023 All rights reserved | Sportslight
+      </div>
     </div>
-  </div>
   );
 }
 export default footer;
-
-
