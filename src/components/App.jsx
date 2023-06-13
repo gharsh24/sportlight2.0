c4cbe544f9f7f829c8152a475fb5014a5a36a160
Neeraj Sharma <neeraj716sharma@gmail.com>
2023-06-13 12:08:58 +0530

    Added Api for Sports Quoute

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const { scrollYProgress } = useScroll();
 
-  return<div style={{ margin: "0 10%" }}>
+  return (
+    <div style={{ margin: "0 10%" }}>
       <motion.div
         className="progress-bar"
         style={{ scaleX: scrollYProgress }}
       />
-
       <Description />
       <Highlight
         src="https://cdn.pixabay.com/photo/2020/03/10/16/47/moon-4919501_640.jpg"
@@ -21,10 +21,10 @@ function App() {
         news="The quick brown fox jumps over the lazy dog and finds a hidden treasure beneath the ancient oak tree near the sparkling river."
       />
       <Latest />
-
       <Latest />
-      <Latest/>;
-      </div>
+      <Latest />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,22 +1,54 @@
+import React, { useEffect, useState } from "react";
+
 function Description() {
-  function Description() {
-    return (
-      <div
-        style={{
-          margin: "5% 0",
-          textAlign: "center",
-          backgroundColor: "#C2DEDC",
-          margin: "3.5% 2.1%",
-          padding: "5%",
-          borderRadius: "20px",
-          fontFamily: "'Montserrat','sans-serif'",
-        }}
-      >
-        <h1>Shining a Spotlight on Sports </h1>
-        <h2>Your source for latest News and Highlights</h2>
-      </div>
-    );
-  }
+  const [quote, setQuote] = useState(null);
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true; // Flag to track component mounting
+
+    fetch("https://api.quotable.io/random?tags=sports")
+      .then((response) => response.json())
+      .then((data) => {
+        if (isMounted) {
+          setQuote(data.content);
+          setAuthor(data.author);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+    // Clean up function to set isMounted to false when the component unmounts
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        margin: "5% 0",
+        textAlign: "center",
+        backgroundColor: "#C2DEDC",
+        margin: "3.5% 2.1%",
+        padding: "5%",
+        borderRadius: "20px",
+        fontFamily: "'Montserrat','sans-serif'",
+      }}
+    >
+      <h1>Shining a Spotlight on Sports </h1>
+      <h2>Your source for latest News and Highlights</h2>
+      {quote ? (
+        <div>
+          <blockquote style={{ fontWeight: "bold" }}>{quote}</blockquote>
+          <h7>~{author}</h7>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
 export default Description;
